88e7ad845e36545a05b99bce5347d2cdf47e7c61
vulebaolong <vulebaolong@gmail.com>
2023-06-03 00:46:38 +0700

    done

--- src/FormValidate/FormValidate.jsx
+++ src/FormValidate/FormValidate.jsx
@@ -1,6 +1,96 @@
 import React, { Component } from "react";
 
 export default class FormValidate extends Component {
+    state = {
+        values: {
+            id: "",
+            image: "",
+            name: "",
+            type: "",
+            price: "",
+            description: "",
+        },
+        errors: {
+            id: "",
+            image: "",
+            name: "",
+            type: "",
+            price: "",
+            description: "",
+        },
+    };
+    handleChangeValue = (e) => {
+        const { name, value } = e.target;
+        let error = "";
+
+        // id
+        if (name === "id") {
+            const reg = /^\d+$/;
+            if (!reg.test(value)) {
+                error = `${name} phải là số`;
+            }
+            if (reg.test(value)) {
+                error = ``;
+            }
+        }
+
+        // imgae
+        if (name === "image") {
+            const urlRegex = /(https?:\/\/[^\s]+)/g;
+            if (!urlRegex.test(value)) {
+                error = `${name} phải đường dẫn`;
+            }
+            if (urlRegex.test(value)) {
+                error = ``;
+            }
+        }
+
+        // name
+        if (name === "name") {
+            const nameRegex =
+                /[^a-zA-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]/u;
+
+            if (nameRegex.test(value)) {
+                error = `${name} phải là chữ`;
+            }
+            if (!nameRegex.test(value)) {
+                error = ``;
+            }
+        }
+
+        // type
+        if (name === "type") {
+            if (value === "0") {
+                console.log("rỗng nè");
+                error = `${name} không đcợc để trống`;
+            }
+        }
+
+        // price
+        if (name === "price") {
+            const reg = /^\d+$/;
+            if (!reg.test(value)) {
+                error = `${name} phải là số`;
+            }
+            if (reg.test(value)) {
+                error = ``;
+            }
+        }
+
+        if (value.trim() === "") {
+            error = `${name} không đcợc để trống`;
+        }
+
+        this.setState(
+            {
+                values: { ...this.state.values, [name]: value },
+                errors: { ...this.state.errors, [name]: error },
+            },
+            () => {
+                console.log(this.state);
+            }
+        );
+    };
     render() {
         return (
             <div className="row row-gap-5">
@@ -11,9 +101,12 @@ export default class FormValidate extends Component {
                             className="form-control"
                             placeholder="id"
                             name="id"
+                            onChange={this.handleChangeValue}
                         />
                         <label>id</label>
-                        <span className="text-danger fst-italic">*</span>
+                        <span className="text-danger fst-italic">
+                            *{this.state.errors.id}
+                        </span>
                     </div>
                 </div>
                 <div className="col-6">
@@ -23,9 +116,12 @@ export default class FormValidate extends Component {
                             className="form-control"
                             placeholder="image"
                             name="image"
+                            onChange={this.handleChangeValue}
                         />
                         <label>image</label>
-                        <span className="text-danger fst-italic">*</span>
+                        <span className="text-danger fst-italic">
+                            *{this.state.errors.image}
+                        </span>
                     </div>
                 </div>
                 <div className="col-6">
@@ -35,19 +131,28 @@ export default class FormValidate extends Component {
                             className="form-control"
                             placeholder="Name"
                             name="name"
+                            onChange={this.handleChangeValue}
                         />
                         <label>Name</label>
-                        <span className="text-danger fst-italic">*</span>
+                        <span className="text-danger fst-italic">
+                            *{this.state.errors.name}
+                        </span>
                     </div>
                 </div>
                 <div className="col-6">
-                    <select name="type" className="form-select form-select-lg">
+                    <select
+                        name="type"
+                        className="form-select form-select-lg"
+                        onChange={this.handleChangeValue}
+                    >
                         <option value={0}>Product Type</option>
                         <option value={1}>One</option>
                         <option value={2}>Two</option>
                         <option value={3}>Three</option>
                     </select>
-                    <span className="text-danger fst-italic">*</span>
+                    <span className="text-danger fst-italic">
+                        *{this.state.errors.type}
+                    </span>
                 </div>
                 <div className="col-6">
                     <div className="form-floating">
@@ -56,9 +161,12 @@ export default class FormValidate extends Component {
                             className="form-control"
                             placeholder="Price"
                             name="price"
+                            onChange={this.handleChangeValue}
                         />
                         <label htmlFor="floatingInput">Price</label>
-                        <span className="text-danger fst-italic">*</span>
+                        <span className="text-danger fst-italic">
+                            *{this.state.errors.price}
+                        </span>
                     </div>
                 </div>
                 <div className="col-6">
@@ -67,9 +175,12 @@ export default class FormValidate extends Component {
                             className="form-control"
                             placeholder="Description"
                             name="description"
+                            onChange={this.handleChangeValue}
                         ></textarea>
                         <label htmlFor="floatingInput">Description</label>
-                        <span className="text-danger fst-italic">*</span>
+                        <span className="text-danger fst-italic">
+                            *{this.state.errors.description}
+                        </span>
                     </div>
                 </div>
             </div>

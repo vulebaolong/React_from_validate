711d3514a14b0e07abfabde84995e6ddab0e13a5
vulebaolong <vulebaolong@gmail.com>
2023-06-03 00:23:29 +0700

    display

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import FormValidate from "./FormValidate/FormValidate";
+
+function App() {
+    return (
+        <div className="container">
+            <h2 className="text-center my-5">Form Validate</h2>
+            <FormValidate />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/FormValidate/FormValidate.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+
+export default class FormValidate extends Component {
+    render() {
+        return (
+            <div className="row row-gap-5">
+                <div className="col-6">
+                    <div className="form-floating">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="id"
+                            name="id"
+                        />
+                        <label>id</label>
+                        <span className="text-danger fst-italic">*</span>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <div className="form-floating">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="image"
+                            name="image"
+                        />
+                        <label>image</label>
+                        <span className="text-danger fst-italic">*</span>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <div className="form-floating">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Name"
+                            name="name"
+                        />
+                        <label>Name</label>
+                        <span className="text-danger fst-italic">*</span>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <select name="type" className="form-select form-select-lg">
+                        <option defaultValue>Product Type</option>
+                        <option value={1}>One</option>
+                        <option value={2}>Two</option>
+                        <option value={3}>Three</option>
+                    </select>
+                    <span className="text-danger fst-italic">*</span>
+                </div>
+                <div className="col-6">
+                    <div className="form-floating">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Price"
+                            name="price"
+                        />
+                        <label htmlFor="floatingInput">Price</label>
+                        <span className="text-danger fst-italic">*</span>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <div className="form-floating">
+                        <textarea
+                            className="form-control"
+                            placeholder="Description"
+                            name="description"
+                        ></textarea>
+                        <label htmlFor="floatingInput">Description</label>
+                        <span className="text-danger fst-italic">*</span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
